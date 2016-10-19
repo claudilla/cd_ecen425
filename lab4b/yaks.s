ae83a3e48f1990bc599c6c5ae301d1e662f265cc
claudilla <claudia.scruzr@gmail.com>
2016-10-19 04:06:08 -0600

    new changes

--- lab4b/yaks.s
+++ lab4b/yaks.s
@@ -1,32 +1,28 @@
 YKEnterMutex:
+    push bp
+    mov bp, sp
+
     cli ;disable interrupts
+
+    mov sp, bp
+    pop bp
+
+
     ret ;return from procedure or function
 
 
 YKExitMutex:
+
+    push bp
+    mov bp, sp
+
     sti ;enble interrupts
+
+    mov sp, bp
+    pop bp
     ret ;return from procedure or function
 
 
-YKDispatcher_one:
-    mov bx, word[YKReadyNextTask]  ; switch the top of the stack to next task to run
-    mov sp,[bx]
-
-    mov bx, word[YKReadyNextTask]   ; move ready task to the runnin task
-    mov word[YKcurrTask], bx   
-
-	pop bp			; pop off reverse order, restore context
-	pop es		
-	pop ds
-	pop di
-	pop si
-
-    pop DX
-	pop CX
-	pop BX
-	pop AX
-
-    iret
 
 
 YKDispatcher:
@@ -66,6 +62,25 @@ YKDispatcher:
 	pop AX
     iret
 
+YKDispatcher_one:
+    mov bx, word[YKReadyNextTask]  ; switch the top of the stack to next task to run
+    mov sp, [bx]
+
+    mov bx, word[YKReadyNextTask]   ; move ready task to the runnin task
+    mov word[YKcurrTask], bx   
+
+	pop bp			; pop off reverse order, restore context
+	pop es		
+	pop ds
+	pop di
+	pop si
+
+    pop DX
+	pop CX
+	pop BX
+	pop AX
+
+    iret
 
 
 
