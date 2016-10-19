0446972f94ddff0a7dd86aa3420921e6904bbd2a
claudilla <claudia.scruzr@gmail.com>
2016-10-19 04:36:32 -0600

    testing

--- lab4b/yakk.h
+++ lab4b/yakk.h
@@ -8,25 +8,25 @@
 typedef struct TCBlock *TCBptr; //pointer that points to the struct??
 
 typedef struct TCBlock {				
-	//int	TCBId;			/* Task name or ID*/
+	int	TCBId;			/* Task name or ID*/    
 	void *stackptr;		/* pointer to current top of stack */
 	//void *ip;	// void (* task)(void)
     char state;			/* current state */
-    unsigned int priority;		/* current priority */
-    unsigned int delay;
+    int priority;		/* current priority */
+    int delay;
     TCBptr next;		/* forward ptr for double linked list */
     TCBptr prev;       /* prev otr for double linked lsit*/
 }  TCB;
 
 extern int YKCtxSwCount;
-//extern int YKIdleCount;
+
 typedef unsigned int UWORD;
 
 void YKInitialize(void);
 void YKIdleTask(void);
 void YKNewTask(void (* task)(void), void *taskStack, int priority);
 void YKRun(void);
-//void YKDispatcher_one(void);
+void YKDispatcher_one(void);
 void YKDispatcher();
 void YKEnterMutex(void);
 void YKExitMutex(void);
